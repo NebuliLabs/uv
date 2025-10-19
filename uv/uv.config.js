4e23f45be10b10d359ea6b897f04eb2fe57a6fc1
Aquanix <68292357+NebuliLabs@users.noreply.github.com>
2025-10-19 16:45:11 -0500

    Add files via upload

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+	prefix: "/p/ultraviolet/ultraviolet/",
+	bare: "https://meximathurbanned.global.ssl.fastly.net/tspmo/",
+	encodeUrl: Ultraviolet.codec.xor.encode,
+	decodeUrl: Ultraviolet.codec.xor.decode,
+	handler: "/p/ultraviolet/uv/uv.handler.js",
+	bundle: "/p/ultraviolet/uv/uv.bundle.js",
+	config: "/p/ultraviolet/uv/uv.config.js",
+	sw: "/p/ultraviolet/uv/uv.sw.js",
+};
